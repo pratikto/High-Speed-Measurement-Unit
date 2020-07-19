72081afde5bbc438646d30d97776f20650cdf700
Pratikto <pratikto.sulthoni.h@gmail.com>
2020-07-19 07:58:24 +0700

    connect mux4to1 input output
    Port, Z0 => Mux, A(0)
    Port, Z1 => Mux, A(1)
    Port, Z2 => Mux, A(2)
    Port, Z3 => Mux, A(3)
                         Mux, B => Count0, Z
                         Mux, B => Count1, Z
                         Mux, B => Count2, Z
                         Mux, B => Count3, Z

--- DAQ/HDL/mux4to1.vhdl
+++ DAQ/HDL/mux4to1.vhdl
@@ -2,18 +2,16 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 entity mux4 is
 port(
-  a1      : in  std_logic_vector(2 downto 0);
-  a2      : in  std_logic_vector(2 downto 0);
-  a3      : in  std_logic_vector(2 downto 0);
-  a4      : in  std_logic_vector(2 downto 0);
+  a       : in  std_logic_vector(3 downto 0);
   sel     : in  std_logic_vector(1 downto 0);
-  b       : out std_logic_vector(2 downto 0));
+  b       : out std_logic);
 end mux4;
+
 architecture rtl of mux4 is
   -- declarative part: empty
 begin
-  b <= a1 when (sel = "00") else
-       a2 when (sel = "01") else
-       a3 when (sel = "10") else
-       a4;
+  b <= a(1) when (sel = "00") else
+       a(2) when (sel = "01") else
+       a(3) when (sel = "10") else
+       a(4);
 end rtl;
