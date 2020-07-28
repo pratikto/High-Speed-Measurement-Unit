65d93fc55700c01987905dfd9a9285460d22faa0
Pratikto <pratikto.sulthoni.h@gmail.com>
2020-07-28 15:10:18 +0700

    fix mux4to1

--- DAQ/HDL/mux4to1.vhdl
+++ DAQ/HDL/mux4to1.vhdl
@@ -2,20 +2,16 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 entity mux4 is
 port(
-  a       : in  std_logic_vector(7 downto 0);
-  sel     : in  std_logic_vector(2 downto 0);
+  a       : in  std_logic_vector(3 downto 0);
+  sel     : in  std_logic_vector(1 downto 0);
   b       : out std_logic);
 end mux4;
 
 architecture rtl of mux4 is
   -- declarative part: empty
 begin
-  b <= a(0) when (sel = "000") else
-       a(1) when (sel = "001") else
-       a(2) when (sel = "010") else
-       a(3) when (sel = "011") else
-       a(4) when (sel = "100") else
-       a(5) when (sel = "101") else
-       a(6) when (sel = "110") else
-       a(7);
+  b <= a(0) when (sel = "00") else
+       a(1) when (sel = "01") else
+       a(2) when (sel = "10") else
+       a(3);
 end rtl;
