075e8be68e6eb8d41200c6af23041c1f0f3a7b3c
Pratikto <pratikto.sulthoni.h@gmail.com>
2020-08-03 13:48:19 +0700

    Rename port
    SCLR to Clr

--- DAQ/HDL/CounterUp16bit.vhd
+++ DAQ/HDL/CounterUp16bit.vhd
@@ -37,7 +37,7 @@ entity CounterUp16bit is
    port( 
         CE: in std_logic;
         Clk: in std_logic;
- 	    SCLR: in std_logic;
+ 	    Clr: in std_logic;
  	    Q: out std_logic_vector(15 downto 0)
     );
 end CounterUp16bit;
@@ -45,9 +45,9 @@ end CounterUp16bit;
 architecture Behavioral of CounterUp16bit is
    signal temp: std_logic_vector(15 downto 0);
 begin
-    process(Clk,SCLR)
+    process(Clk,Clr)
     begin
-        if SCLR = '1' then
+        if Clr = '1' then
             temp <= "0000000000000000";
         elsif(rising_edge(Clk) and CE = '0') then
            if temp = "1111111111111111" then
